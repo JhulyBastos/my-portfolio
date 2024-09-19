849572c89b6c036496be9984085e09c584081857
Jhuly Bastos <jhulytbds@gmail.com>
2024-09-19 10:59:22 -0300

    responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
       {/* parte de cima */}
       <header
         id="inicio"
-        className="bg-dark-20 justify-between flex items-center px-20 py-6"
+        className="bg-dark-20 justify-between flex items-center lg:px-20 sm:px-10 py-6 sm:gap-2"
       >
         <div>
-          <h1 className="text-brand-color text-2xl">{`<Jhuly Bastos/>`}</h1>
+          <h1 className="text-brand-color xl:text-2xl sm:text-xl">{`<Jhuly Bastos/>`}</h1>
         </div>
         <div>
-          <nav>
+          <nav className="lg:text-base sm:text-sm">
             <ol className="flex justify-center items-center gap-7">
               <li>
                 <a href="#inicio">Inicio</a>
@@ -37,12 +37,14 @@ export default function Home() {
       {/* parte de cima acaba aq */}
 
       {/* meio */}
-      <div className=" flex items-center justify-between w-full max-w-[1300px] mx-auto px-20  ">
+      <div className=" flex items-center justify-between w-full max-w-[1300px] mx-auto xl:px-20 md:px-14 sm:px-10   ">
         <div className="flex flex-col justify-start gap-1 max-w-full max-h-full ">
           <div>
             <p>Olá, eu sou</p>
-            <h1 className="text-5xl text-brand-color">Jhuly Bastos</h1>
-            <p className="mt-4">Desenvolvedora Front-end e Designer</p>
+            <h1 className="xl:text-5xl md:text-4xl sm:text-3xl text-brand-color">
+              Jhuly Bastos
+            </h1>
+            <p className="mt-4 ">Desenvolvedora Front-end e Designer</p>
           </div>
           <div className="flex gap-2 ">
             <a
@@ -59,7 +61,7 @@ export default function Home() {
         <div className="mt-4  hover:scale-105 duration-300 ">
           <a href="https://github.com/JhulyBastos" target="_blank">
             <img
-              className=" lg:w-[400px] lg:h-[400px] sm:w-[300px] sm:h-[300px]  w-[200px] h-[200px] rounded-full border-brand-color border-4 p-2 mt-12"
+              className=" lg:w-[400px] lg:h-[400px] md:w-[320px] md:h-[320px] sm:w-[250px] sm:h-[250px]  w-[200px] h-[200px] rounded-full border-brand-color border-4 p-2 mt-12"
               src="/assets/imagens/perfil.jpg"
               alt="Imagem de perfil"
             />
@@ -70,7 +72,7 @@ export default function Home() {
         <h1 id="sobre-mim" className="text-2xl text-brand-color">
           Sobre mim
         </h1>
-        <p className="w-1/2 text-[16px] text-center mt-5">
+        <p className="lg:w-1/2 md:w-4/5 sm:w-4/5 text-[16px]   text-center mt-5">
           Entusiasta da tecnologia, com foco no desenvolvimento front-end, busco
           aprimorar constantemente minhas habilidades para oferecer experiências
           digitais envolventes e intuitivas. Sou graduada em Design gráfico e
